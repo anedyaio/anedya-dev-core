e61318859bbf3b34a568e577626833139d44b8bf
suraj maurya <mnaveen1888@gmail.com>
2024-10-28 13:17:55 +0530

    add Documentation -> anedya_config_init()

--- include/anedya_config.h
+++ include/anedya_config.h
@@ -48,9 +48,8 @@ extern "C"
          *                     It must contain 32 hexadecimal digits separated by hyphens.
          * @param[out] devID   An `anedya_device_id_t` array (16 bytes) that receives the parsed binary device ID.
          *
-         * @return
-         *     - `ANEDYA_OK` on successful parsing.
-         *     - `ANEDYA_ERR_INVALID_DEVICE_ID` if the input `deviceID` is incorrectly formatted.
+         * @retval `ANEDYA_OK` on successful parsing.
+         * @retval `ANEDYA_ERR_INVALID_DEVICE_ID` if the input `deviceID` is incorrectly formatted.
          *
          * @note This function requires the device ID string to be exactly 36 characters in length, including
          *       hyphens. Each hexadecimal character is validated, and invalid characters result in an error.
@@ -59,11 +58,32 @@ extern "C"
          */
         anedya_err_t anedya_parse_device_id(const char deviceID[37], anedya_device_id_t devID);
 
-        // Config management APIs
-        /** @brief: Initialize a config with default values*/
+        /**
+         * @brief Initializes the configuration structure for an Anedya client.
+         *
+         * This function sets up the `anedya_config_t` structure by assigning the connection key,
+         * connection key length, and device ID. It also converts the binary device ID (`devId`)
+         * into a UUID string format and stores it in the `_device_id_str` field of the configuration.
+         * Additionally, all event handlers are initialized to `NULL`.
+         *
+         * @param[out] config            Pointer to the `anedya_config_t` structure to initialize.
+         * @param[in]  devId             The binary device ID in the form of `anedya_device_id_t`.
+         * @param[in]  connection_key    Pointer to a string containing the connection key.
+         * @param[in]  connection_key_len Length of the connection key string.
+         *
+         * @return
+         *      - `ANEDYA_OK` if initialization is successful.
+         *
+         * @note This function sets the event handler, on_connect, and on_disconnect callbacks to `NULL`.
+         *       Ensure to set these callbacks if needed after initialization.
+         *
+         * @warning The `config` pointer must point to valid memory, as this function will write data
+         *          directly to the fields of `config`.
+         */
         anedya_err_t anedya_config_init(anedya_config_t *config, anedya_device_id_t devId, const char *connection_key, size_t connection_key_len);
-        /** @brief: TODO */
+
         anedya_err_t anedya_config_set_region(anedya_config_t *config, const char *region);
+
         /** @brief: TODO */
         anedya_err_t anedya_config_set_timeout(anedya_config_t *config, size_t timeout_sec);
 
