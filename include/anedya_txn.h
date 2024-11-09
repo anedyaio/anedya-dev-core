4677e10df4c4f95650185ce6fbf41c9de01e7a01
suraj maurya <mnaveen1888@gmail.com>
2024-11-09 15:12:57 +0530

    add documentation for anedya_txn_register_callback(anedya_txn_t *txn, anedya_txn_cb_t callback, anedya_context_t ctx);

--- include/anedya_txn.h
+++ include/anedya_txn.h
@@ -14,42 +14,57 @@ extern "C"
 {
 #endif
 
-typedef uint8_t anedya_txn_desc_t;
-typedef size_t anedya_op_t;
-typedef size_t anedya_op_err_t;
-typedef void *anedya_response_t;
-typedef void (*anedya_txn_cb_t)(anedya_txn_t *txn, anedya_context_t ctx);
+    typedef uint8_t anedya_txn_desc_t;
+    typedef size_t anedya_op_t;
+    typedef size_t anedya_op_err_t;
+    typedef void *anedya_response_t;
+    typedef void (*anedya_txn_cb_t)(anedya_txn_t *txn, anedya_context_t ctx);
 
-struct anedya_txn_t
-{
-    anedya_txn_desc_t desc;
+    struct anedya_txn_t
+    {
+        anedya_txn_desc_t desc;
 #ifdef ANEDYA_ENABLE_DYNAMIC_ALLOCATION
-    char *_txbody;
-    char *_rxbody;
+        char *_txbody;
+        char *_rxbody;
 #endif
 #ifdef ANEDYA_ENABLE_STATIC_ALLOCATION
-    char _rxbody[ANEDYA_RX_BUFFER_SIZE];
+        char _rxbody[ANEDYA_RX_BUFFER_SIZE];
 #endif
-    size_t _rx_len;
-    bool is_complete;
-    bool is_success;
-    anedya_context_t ctx;
-    anedya_txn_cb_t callback;
-    anedya_op_t _op;
-    anedya_err_t _op_err;
-    anedya_response_t response;
-};
+        size_t _rx_len;
+        bool is_complete;
+        bool is_success;
+        anedya_context_t ctx;
+        anedya_txn_cb_t callback;
+        anedya_op_t _op;
+        anedya_err_t _op_err;
+        anedya_response_t response;
+    };
 
-/** @brief: TODO */
-void anedya_txn_register_callback(anedya_txn_t *txn, anedya_txn_cb_t callback, anedya_context_t ctx);
+    /**
+     * @brief Register a callback function for a transaction.
+     *
+     * This function associates a user-defined callback function and context with a specific transaction.
+     * The callback will be triggered when the transaction completes, allowing for custom handling of
+     * transaction results.
+     *
+     * @param[in,out] txn Pointer to the `anedya_txn_t` structure representing the transaction.
+     * @param[in] callback The callback function to be registered for the transaction. The callback will
+     *                     be executed upon completion of the transaction.
+     * @param[in] ctx User-defined context to be passed to the callback function. This context can be
+     *                used to store additional information or parameters needed within the callback.
+     *
+     * @retval None
+     *
+     * @note Ensure that the callback and context are valid before assigning them to the transaction.
+     */
+    void anedya_txn_register_callback(anedya_txn_t *txn, anedya_txn_cb_t callback, anedya_context_t ctx);
 
-anedya_err_t _anedya_txn_register(anedya_client_t *client, anedya_txn_t *txn);
-anedya_err_t _anedya_txn_complete(anedya_client_t *client, anedya_txn_t *txn);
-anedya_txn_desc_t _anedya_txn_get_desc(anedya_txn_t *txn);
+    anedya_err_t _anedya_txn_register(anedya_client_t *client, anedya_txn_t *txn);
+    anedya_err_t _anedya_txn_complete(anedya_client_t *client, anedya_txn_t *txn);
+    anedya_txn_desc_t _anedya_txn_get_desc(anedya_txn_t *txn);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* _ANEDYA_TXN_H_ */
-
